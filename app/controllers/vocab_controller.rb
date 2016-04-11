b7d26bbc4cf20aaef654b40dc9f4538134bc692c
Mike Zhang <ubik00@yahoo.com>
2016-04-11 16:25:18 +0200

    added view files and vocab_controller

---
+++ app/controllers/vocab_controller.rb
@@ -0,0 +1,50 @@
+class VocabController < ApplicationController
+
+  def index
+    @vocabs = Vocab.all.order("word")
+  end
+
+  def show
+    @vocab = Vocab.find(params[:id])
+
+  end
+
+  def new
+    @vocab = Vocab.new
+  end
+
+  def edit
+    @vocab = Vocab.find(params[:id])
+  end
+
+  def create
+    @vocab = Vocab.new(vocab_params)
+
+    if @vocab.save
+      redirect_to @vocab #redirect to show page for that word since the id is inside
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @vocab = Vocab.find(params[:id])
+
+    if @vocab.update(vocab_params)
+      redirect_to @vocab
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @vocab = Vocab.find(params[:id])
+    @vocab.destroy
+    redirect_to vocab_index_path
+  end
+
+  private
+    def vocab_params
+      params.require(:vocab).permit(:word, :definition)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #dont use Vocab, use vocab
+   resources :vocab
+   root 'vocab#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
